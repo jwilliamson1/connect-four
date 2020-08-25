854c389f6b5010fea4180c3dcb1c48751f92702e
Joseph Williamson <jwilliamson@kbra.com>
2020-08-25 18:47:03 -0400

    play with Kont more

--- src/Kont.hs
+++ src/Kont.hs
@@ -17,4 +17,23 @@ instance Monad (Cont r) where
     return = pure
     (Cont inC) >>= fn = Cont $ \out -> inC(\a -> runCont(fn a) out)
 
-callCC :: ((a -> Cont r b)) -> Cont r a) -> Cont r a
+callCC :: ((a -> Cont r b) -> Cont r a) -> Cont r a
+callCC fn = Cont $ \out -> runCont (fn (\a -> Cont $ \_ -> out a)) out
+
+cont :: ((a -> r) -> r) -> Cont r a
+cont = Cont
+
+twoC' :: Cont r Integer
+twoC' = return 2
+helloC' :: Cont r [Char]
+helloC' = return "hello"
+
+twoHelloC' = do
+    two <- twoC'
+    hello <- helloC'
+    return $ (show two)++hello
+
+twoMultiC' = do
+    two <- Cont $ \out -> out 2 ++ out 2
+    hello <- helloC'
+    return $ (show two)++hello
