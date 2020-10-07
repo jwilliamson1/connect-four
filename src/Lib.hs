5516454e7c952cfec292789eb97cc7e0a10c4307
Alia Bellamy <aliabellamy@Alias-MacBook-Pro.local>
2020-10-07 06:54:45 -0400

    diagonal reporting wins in both directions but not correct winning positions

--- src/Lib.hs
+++ src/Lib.hs
@@ -9,8 +9,6 @@ data PlayState = Play | End
 type Space = Maybe Player
 type Board = [[Space]] -- a list of columns
 
--- addChip :: Board -> Int -> a -> Boards
--- addChip board col marker = !!
 start :: IO ()
 start = playLoop Play
 
@@ -28,22 +26,23 @@ playLoop Play = do
 
 playGame :: Player -> Player -> Board -> IO ()
 playGame t1 t2 b =
-                     if detectFilled b
-                        then do
-                            putStrLn "IT'S A TIE! NO MORE SPACES AVAILABLE!"
-                            printBoard b
-                        else
-                            do
-                                putStrLn $ show t1 ++ " enter column 0-6 to which to add."
-                                move <- safeReadInt :: IO Int
-                                b'<- retryingAddToken b move t1
-                                let maybeWin = boardHasWin t1 b'
-                                case maybeWin of 
-                                    (Just (runLength, positions)) -> do
-                                        putStrLn $ show t1 ++ " wins!"
-                                        printBoard b'
-                                    Nothing -> do printBoard b'
-                                                  playGame t2 t1 b' 
+    if detectFilled b
+    then do
+        putStrLn "IT'S A TIE! NO MORE SPACES AVAILABLE!"
+        printBoard b
+    else
+        do
+            putStrLn $ show t1 ++ " enter column 1-7 to which to add."
+            move <- safeReadInt :: IO Int
+            b'<- retryingAddToken b (move - 1) t1
+            let maybeWin = boardHasWin t1 b'
+            case maybeWin of 
+                (Just (runLength, positions)) -> do
+                    putStrLn $ show t1 ++ " wins!"
+                    printBoard b'
+                    putStrLn $ "Winning positions " ++ (show $ reverse positions)
+                Nothing -> do printBoard b'
+                              playGame t2 t1 b' 
                
 safeReadInt :: IO Int
 safeReadInt = do 
@@ -52,7 +51,7 @@ safeReadInt = do
     case attempt of
         (Just r) -> return r
         (_) -> do
-            putStrLn "Invalid input, must be between 0-5."
+            putStrLn "Invalid input, must be between 1-7."
             safeReadInt
 
 retryingAddToken :: Board -> Int -> Player -> IO Board
@@ -61,7 +60,7 @@ retryingAddToken b c p = let attempt = addToken b c p
                              (Left msg) -> do
                                  putStrLn msg
                                  move <- safeReadInt
-                                 retryingAddToken b move p
+                                 retryingAddToken b (move -1) p
                              (Right b) -> return b
 
 replace :: [a] -> Int -> a -> [a]
@@ -71,7 +70,7 @@ replace (l:ls) i el = l : replace ls (i-1) el
 
 addToken :: Board -> Int -> Player -> Either [Char] Board
 addToken board col player =
-    if col < 0 || col > 6 then Left $ show player ++ "column must be between 0 and 6." else
+    if col < 0 || col > 6 then Left $ show player ++ "column must be between 1 and 7." else
     let selectedColumn = board !! col
     in if canAddToColumn selectedColumn
         then Right $ replace board col $ addToColumn selectedColumn player
@@ -112,71 +111,76 @@ detectFilled b = all columnFull b
           isPlayer sp = case sp of
               Nothing -> False
               _ -> True
-boardHasWin :: Player -> Board -> Maybe (Int, [Int])
+
+boardHasWin :: Player -> Board -> Maybe (Int, [(Int, Int)])
 boardHasWin p b = let colWin = columnsHaveWin p b
                       rowWin = rowsHaveWin p b
                       diagonalLeftDownWin = diagonalLeftDownHasWin p b
                       diagonalRightUpWin = diagonalRightUpHasWin p b
                       wins = [colWin, rowWin, diagonalLeftDownWin, diagonalRightUpWin]
                       in msum wins
-rowsHaveWin p b = columnsHaveWin p $ transpose b
-
+rowsHaveWin p b = let maybeWin = columnsHaveWin p $ transpose b
+                    in fmap reversePositions maybeWin
+                    where swap (r,c) = (c, r) 
+                          reversePositions :: (Int, [(Int, Int)]) -> (Int, [(Int, Int)])                          
+                          reversePositions (run, f) = (run, fmap swap f)
+                            
 diagonalLeftDownHasWin p b = diagonalRightUpHasWin p $ reverse b
 
-diagonalRightUpHasWin :: Player -> Board -> Maybe (Int, [Int])
-diagonalRightUpHasWin p b = slideRight 0 3 b
+diagonalRightUpHasWin :: Player -> Board -> Maybe (Int, [(Int, Int)])
+diagonalRightUpHasWin p b = slideRight 1 4 b
                           where slideRight s e cols = if s == (e + 1) then Nothing
-                                else let downRes = slideDown 0 2 p cols s
+                                else let downRes = slideDown 1 3 p cols s
                                         in case downRes of
                                             (Just a) -> Just a
                                             _ -> slideRight (s + 1) e (drop 1 cols) 
 
-slideDown :: Int -> Int -> Player -> [[Space]] -> Int -> Maybe (Int, [Int])
+slideDown :: Int -> Int -> Player -> [[Space]] -> Int -> Maybe (Int, [(Int, Int)])
 slideDown sr er p cols' curCol = if sr == (er + 1) then Nothing
 else let diagRes = checkDiagonal4 sr curCol p cols'
         in case diagRes of 
             (Just a) -> Just a
             _ -> slideDown (sr + 1) er p (fmap tail cols') curCol 
 
-checkDiagonal4 :: Int -> Int -> Player -> [[Space]] -> Maybe (Int, [Int])
+checkDiagonal4 :: Int -> Int -> Player -> [[Space]] -> Maybe (Int, [(Int, Int)])
 checkDiagonal4 r c p b = let playersMatch a b c' d = a == p && b == p && c' == p && d == p
                              c1 = b !! 0 !! 0
                              c2 = b !! 1 !! 1
                              c3 = b !! 2 !! 2
                              c4 = b !! 3 !! 3
                              in let res = playersMatch <$> c1 <*> c2 <*> c3 <*> c4
-                                in res >>= (\r' -> if r' then Just (r, [c, c+1, c+2, c+3]) else Nothing)
+                                in res >>= (\match -> if match then Just (4, [(r+3, c+3), (r+2, c+2),  (r+1, c+1), (r,c) ]) else Nothing)
 
 
-columnsHaveWin :: Player -> Board -> Maybe (Int, [Int])
+columnsHaveWin :: Player -> Board -> Maybe (Int, [(Int, Int)])
 columnsHaveWin p b = let checkForPlayer = lineHasWin p
-                    in let maybeWins = fmap checkForPlayer b
+                    in let maybeWins = fmap checkForPlayer $ zip b [1..]
                            winningLines = filter hasWin maybeWins
                            in if length winningLines > 0 then head winningLines else Nothing 
                         where 
-                            hasWin :: Maybe (Int, [Int]) -> Bool
+                            hasWin :: Maybe (Int, [(Int, Int)]) -> Bool
                             hasWin m = case m of
                                 Just _ -> True
                                 Nothing -> False
 
-lineHasWin :: Player -> [Space] -> Maybe (Int, [Int])
-lineHasWin p sps =
-  let res = detectWin p (zip sps [1 ..])
+lineHasWin :: Player -> ([Space], Int) -> Maybe (Int, [(Int, Int)])
+lineHasWin pl (sps, col) =
+  let res = detectWin pl col (zip sps [1 ..])
    in if fst res > 3 then Just res else Nothing
 
-detectWin :: Player -> [(Space, Int)] -> (Int, [Int])
-detectWin pl sps = foldl' accumRun (0, []) sps
+detectWin :: Player -> Int -> [(Space, Int)] -> (Int, [(Int, Int)])
+detectWin pl const sps = foldl' accumRun (0, []) sps
   where
     reset = (0, [])
-    accumRun :: (Int, [Int]) -> (Space, Int) -> (Int, [Int])
+    accumRun :: (Int, [(Int, Int)]) -> (Space, Int) -> (Int, [(Int, Int)])
     accumRun (count, run) sp =
       let alreadyWon = (length run) >= 4
        in case sp of
-            (Nothing, _) -> if alreadyWon then (count,run) else reset
+            (Nothing, _) -> if alreadyWon then (count, run) else reset
             (Just aplayer, pos) ->
               if pl == aplayer
-                then (count + 1, pos : run)
-                else if alreadyWon then (count,run) else reset
+                then (count + 1, (pos, const) : run)
+                else if alreadyWon then (count, run) else reset
 
 printEitherBoard :: Either [Char] Board -> IO ()
 printEitherBoard x = case x of Left err -> putStrLn err; Right b -> printBoard b
